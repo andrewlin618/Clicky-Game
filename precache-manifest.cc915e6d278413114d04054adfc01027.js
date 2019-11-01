f0330937b3153b11e66a11df4b2e94ce924fee32
Andrew Lin <andrewlin618@gmail.com>
2019-10-31 19:59:37 -0700

    Updates

---
+++ precache-manifest.cc915e6d278413114d04054adfc01027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b6ae90d13069847c6b77e0a30df7c4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "07627a05c65cff083001",
+    "url": "/clicky-game/static/css/main.09797b28.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "07627a05c65cff083001",
+    "url": "/clicky-game/static/js/main.616d6c54.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
